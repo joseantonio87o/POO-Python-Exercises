41f0776936a8351a23ed7c385ffadfb1f9a3b501
Ortega Diaz <jose-antonio.ortega-diaz@sogeti.com>
2024-10-21 13:31:26 +0200

    Updated version to v1.2 in the script

--- theory/01_classes_and_objects.py
+++ theory/01_classes_and_objects.py
@@ -1,11 +1,12 @@
 # 01_classes_and_objects.py
 # ------------------------------------------------------
 # Project: POO-Python-Exercises
-# Version: 1.1
+# Version: 1.2 # Updated to v1.2
 # Last Updated: 2024-10-21
 # Author: Jose Antonio
 # ------------------------------------------------------
 # Change Log:
+# v1.2 - Translated final comment and updated script version to 1.2
 # v1.1 - Translated comments, variable names, and function names to English
 # v1.0 - Initial version with basic class structure and comments in Spanish
 # ------------------------------------------------------
